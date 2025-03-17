baf4594cb1ccdada16a9455490de29ba753eff27
maxjlaz1 <maxjlaz@icloud.com>
2025-03-17 09:37:54 -0400

    cleanup

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,8 +1,10 @@
 #include <Servo.h>
 #include <stdio.h>
 #include <string.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
 #include "Arduino.h"
-#include <SoftwareSerial.h>;
+#include <SoftwareSerial.h>
 
 const int RS_RO = 8;
 const int RS_DI = 12;
@@ -19,7 +21,7 @@ String data[String_Count];
 String msg = "";
 bool inMsg = false;
 
-SoftwareSerial RS_SLAVE(RS_RO, RS_DI); //RX, TX
+SoftwareSerial RS_SLAVE(RS_RO, RS_DI);  //RX, TX
 
 float LeftJoystickY = 0.0;
 float LeftJoystickX = 0.0;
@@ -38,20 +40,16 @@ bool Down = false;
 bool Left = false;
 bool Right = false;
 
+int motMinPWM = 1220;
+int motMaxPWM = 1780;
+int serMinPWM = 184;
+int serMaxPWM = 430;
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVO_FREQ 50  // Analog servos run at ~50 Hz
+
 void setup() {
-  //https://learn.adafruit.com/16-channel-pwm-servo-driver/hooking-it-up
-  //pwm.setPWMFreq(400);
-
-  foward_left.attach(3);
-  foward_right.attach(5);
-  up_left.attach(6);
-  up_right.attach(9);
-
-  foward_left.write(0);
-  foward_right.write(0);
-  up_left.write(0);
-  up_right.write(0);
-
   Serial.begin(9600);
   Serial.println("BOOT");
   RS_SLAVE.begin(9600);
@@ -60,15 +58,21 @@ void setup() {
   Serial.flush();
   delay(1000);
   Serial.println("READY");
+
+  pwm.begin();
+  pwm.setOscillatorFrequency(27000000);  // Set the PWM frequency
+  pwm.setPWMFreq(SERVO_FREQ);
+
+  setServosToZero();
 }
 
 void loop() {
-  if(RS_SLAVE.available()){
+  if (RS_SLAVE.available()) {
     char c = RS_SLAVE.read();
-    if(c == '<'){
-      inMsg = true; 
-    } else if(c == '>'){
-      Serial.println(msg);
+    if (c == '<') {
+      inMsg = true;
+    } else if (c == '>') {
+      //Serial.println(msg);
       splitString(msg, '!', data);
       // Serial.print("[");
       // for (int i = 0; i < String_Count; i++) {
@@ -82,58 +86,74 @@ void loop() {
       setServosAndMotors();
       msg = "";
       inMsg = false;
-    } else if(inMsg){
-      msg+=c;
+    } else if (inMsg) {
+      msg += c;
     }
   }
 }
 
-void assignValues(){
-  LeftJoystickX = (data[0].toInt()/1000.0);
-  LeftJoystickY = (data[1].toInt()/1000.0);
-  RightJoystickX = (data[2].toInt()/1000.0);
-  RightJoystickY = (data[3].toInt()/1000.0);
-  LeftTrigger = (data[4].toInt()/100.0);
-  RightTrigger = (data[5].toInt()/100.0);
+void assignValues() {
+  LeftJoystickX = (data[0].toInt() / 1000.0);
+  LeftJoystickY = (data[1].toInt() / 1000.0);
+  RightJoystickX = (data[2].toInt() / 1000.0);
+  RightJoystickY = (data[3].toInt() / 1000.0);
+  LeftTrigger = (data[4].toInt() / 100.0);
+  RightTrigger = (data[5].toInt() / 100.0);
 
-  if((sizeof(data) / sizeof(data[0])>6)){
+  resetButtonBools();
+
+  if ((sizeof(data) / sizeof(data[0]) > 6)) {
     for (int i = 6; i < (sizeof(data) / sizeof(data[0])); i++) {
-      if(data[i]=="a"){
+      if (data[i] == "a") {
         A = true;
-      } else if(data[i]=="b"){
+      } else if (data[i] == "b") {
         B = true;
-      } else if(data[i]=="x"){
+      } else if (data[i] == "x") {
         X = true;
-      } else if(data[i]=="y"){
+      } else if (data[i] == "y") {
         Y = true;
-      } else if(data[i]=="j"){
+      } else if (data[i] == "j") {
         LeftBumper = true;
-      } else if(data[i]=="k"){
+      } else if (data[i] == "k") {
         RightBumper = true;
-      } else if(data[i]=="u"){
+      } else if (data[i] == "u") {
         Up = true;
-      } else if(data[i]=="d"){
+      } else if (data[i] == "d") {
         Down = true;
-      } else if(data[i]=="l"){
+      } else if (data[i] == "l") {
         Left = true;
-      } else if(data[i]=="r"){
+      } else if (data[i] == "r") {
         Right = true;
       }
     }
   }
 }
 
-void setServosAndMotors(){
-  double denom = max(abs(LeftJoystickY) + abs(LeftJoystickX), 1);
-  int fl = abs((LeftJoystickY + LeftJoystickX)/denom)*90;
-  int fr = abs((LeftJoystickY - LeftJoystickX)/denom)*90;
-  int ul = abs(RightJoystickY)*90;
-  int ur = abs(RightJoystickY)*90;
+void resetButtonBools() {
+  LeftBumper = false;
+  RightBumper = false;
+  A = false;
+  X = false;
+  Y = false;
+  B = false;
+  Up = false;
+  Down = false;
+  Left = false;
+  Right = false;
+}
 
-  foward_left.write(fl);
-  foward_right.write(fr);
-  up_left.write(ul);
-  up_right.write(ur);
+void setServosAndMotors() {
+  double denom = max(abs(LeftJoystickY) + abs(LeftJoystickX), 1);
+  int fl = abs((LeftJoystickY + LeftJoystickX) / denom) * 90;
+  int fr = abs((LeftJoystickY - LeftJoystickX) / denom) * 90;
+  int ul = abs(RightJoystickY) * 90;
+  int ur = abs(RightJoystickY) * 90;
+
+  pwm.setPWM(0, 0, positionToPulseServo(ur / 90.0));
+  pwm.setPWM(1, 0, positionToPulseServo(ur / 90.0));
+  pwm.setPWM(2, 0, positionToPulseServo(ul / 90.0));
+  pwm.setPWM(3, 0, positionToPulseServo(fr / 90.0));
+  pwm.setPWM(4, 0, positionToPulseServo(fl / 90.0));
 }
 
 void splitString(String str, char delimiter, String *result) {
@@ -143,28 +163,34 @@ void splitString(String str, char delimiter, String *result) {
 
   while (endIndex >= 0 && partIndex < String_Count) {
     endIndex = str.indexOf(delimiter, startIndex);
-    
+
     if (endIndex == -1) {  // If no more delimiters, grab the rest of the string
       result[partIndex] = str.substring(startIndex);
     } else {
       result[partIndex] = str.substring(startIndex, endIndex);
       startIndex = endIndex + 1;
     }
-    
+
     partIndex++;
   }
 }
 
-// void setServoPos(float speed, int channel) {
-//   float normalizedPos = (speed + 1.0) / 2.0;
-//   float percent = (normalizedPos + 0.40) * 0.80;
-//   int ticks = (int)(percent * 4095);
-//   pwm.setPWM(channel, ticks, 4095-ticks);
-// }
+uint16_t positionToPulseServo(float position) {
+  position = constrain(position, 0.0, 1.0);
+  return serMinPWM + (uint16_t)(position * (serMaxPWM - serMinPWM));
+}
 
-// void setMotorSpeed(float speed, int channel) {
-//   float normalizedSpeed = (speed + 1.0) / 2.0;
-//   float percent = (normalizedSpeed + 0.44) * 0.76;
-//   int ticks = (int)(percent * 4095);
-//   pwm.setPWM(channel, ticks, 4095-ticks);
-// }
+uint16_t positionToPulseMotor(float position) {
+  position = constrain(position, -1.0, 1.0);
+  float normalized = (position + 1.0) / 2.0;
+  return motMinPWM + (uint16_t)(normalized * (motMaxPWM - motMinPWM));
+}
+
+void setServosToZero() {
+  pwm.setPWM(0, 0, positionToPulseServo(0));
+  pwm.setPWM(1, 0, positionToPulseServo(0));
+  pwm.setPWM(2, 0, positionToPulseServo(0));
+  pwm.setPWM(3, 0, positionToPulseServo(0));
+  pwm.setPWM(4, 0, positionToPulseServo(0));
+  delay(100);  // Give servos time to settle
+}
--- sender/Arduino_RS485_Sender/Arduino_RS485_Sender.ino
+++ sender/Arduino_RS485_Sender/Arduino_RS485_Sender.ino
@@ -1,6 +1,7 @@
 #include <SoftwareSerial.h>
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <avr/wdt.h>
 
 const int baudrate = 9600;
 
@@ -23,13 +24,22 @@ RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 bool firstController = true;
 
+uint8_t bufs[4][RH_RF95_MAX_MESSAGE_LEN];
+uint8_t lens[4];
+
+int lastReceivedIndex = 0;
+int lastSentIndex = 0;
+
 void setup() {
+  MCUSR = 0;
+  wdt_disable();
+
   Serial.begin(9600);
   RS_Master.begin(9600);
   pinMode(RS_DE_RE, OUTPUT);
 
   digitalWrite(RS_DE_RE, HIGH);
-  Serial.println("Master ready");
+  //Serial.println("Master ready");
 
     // Reset the module
   pinMode(RFM95_RST, OUTPUT);
@@ -42,18 +52,18 @@ void setup() {
 
   // Initialize the radio
   if (!rf95.init()) {
-    Serial.println("RFM96W LoRa radio init failed");
+    //Serial.println("RFM96W LoRa radio init failed");
     while (1);
   }
-  Serial.println("RFM96W LoRa radio init OK!");
+  //Serial.println("RFM96W LoRa radio init OK!");
 
   // Set frequency
   if (!rf95.setFrequency(RF95_FREQ)) {
-    Serial.println("Failed to set frequency");
+    //Serial.println("Failed to set frequency");
     while (1);
   }
-  Serial.print("Frequency set to: ");
-  Serial.println(RF95_FREQ);
+  //Serial.print("Frequency set to: ");
+  //Serial.println(RF95_FREQ);
 
   // Set receiver gain
   rf95.setTxPower(13, false); // Not used in receiving but good practice
@@ -63,46 +73,67 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //Serial.print("RSSI: ");
+  //Serial.println(rf95.lastRssi(), DEC);
+  rf95.setModeRx();
+  //Serial.write('~');
   if (Serial.available()) {
-    RS_Master.write(Serial.read());
-    //Serial.write('~');
+    char mChar = Serial.read();
+
+    if(mChar == '|'){
+      //Serial.write('R');
+      wdt_enable(WDTO_15MS); // Set watchdog timeout to 15ms
+      while (1);
+    }
+    RS_Master.write(mChar);
+    if(mChar == '>'){
+      for(uint8_t i=0; i<lens[lastSentIndex]; i++){
+        Serial.write((char)bufs[lastSentIndex][i]);
+      }
+      //Serial.write(buf[lastSentIndex], len[lastSentIndex]);
+      lastSentIndex++;
+      lastSentIndex = lastSentIndex == 3 ? 0 : lastSentIndex;
+      Serial.write('~');
+    }
+  }
 
     // if(firstController){
     //   uint8_t data[] = "S";
     //   rf95.send(data, sizeof(data));
-    //   rf95.waitPacketSent();
+    //   //rf95.waitPacketSent();
     //   firstController = false;
     //   delay(1000);
     // }
 
-    // if (rf95.available()) {
-    //   // Buffer to hold received data
-    //   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    //   uint8_t len = sizeof(buf);
-
-    //   // Check if data was received
-    //   if (rf95.recv(buf, &len)) {
-    //     //Serial.println("Message received!");
-    //     //Serial.print("RSSI: ");
-    //     //Serial.println(rf95.lastRssi(), DEC);
-
-    //     // Print the received message
-    //     //Serial.print("Message: ");
-    //     Serial.write(buf, sizeof(buf));
-    //     Serial.write('~');
-    //     // for (uint8_t i = 0; i < len; i++) {
-    //     //   Serial.print((char)buf[i]);
-    //     // }
-    //     //Serial.println();
-    //   } else {
-    //     Serial.println("Receive failed");
-    //   }
-    // }
-    // else{
-    //   Serial.write('~');
-    // }
+  if (rf95.available()) {
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    //Serial.println("Got Radio Data");
+    // Check if data was received
+    if (rf95.recv(buf, &len)) {
+      // Serial.println("Message received!");
+      // Serial.print("RSSI: ");
+      // Serial.println(rf95.lastRssi(), DEC);
+      Serial.write(buf, len);
+      Serial.println();
+      //Print the received message
+      // Serial.print("Message: ");
+      // for (uint8_t i = 0; i < len; i++) {
+      //   Serial.print((char)buf[i]);
+      // }
+      // Serial.println();
+      // memcpy(bufs[lastReceivedIndex], buf, len);
+      // lens[lastReceivedIndex] = len;
+      // Serial.write(bufs[lastReceivedIndex], lens[lastReceivedIndex]);
+      // Serial.println();
+      // lastReceivedIndex++;
+      // lastReceivedIndex = lastReceivedIndex == 3 ? 0 : lastReceivedIndex;
+      //Serial.print((char)buf[0]);
+    } else {
+      //Serial.println("Receive failed");
+    }
+  }else{
+    //Serial.println("No Radio Data");
   }
   //delay(50);
 }
